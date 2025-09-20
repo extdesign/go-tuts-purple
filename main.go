e70b1eeecb1b006180cc183baf0c689a412938ec
Sergey Teterlev <sergey.teterlev@gmail.com>
2025-09-20 21:36:23 +0300

    1-converter result

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+const USD_TO_EURO float64 = 1.20
+const USD_TO_RUB float64 = 64.57
+
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println(convertEuroToRub())
+}
+
+func convertEuroToRub() float64 {
+	return USD_TO_EURO * USD_TO_RUB
 }
