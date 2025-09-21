79500a8b93876df54fd1178ad200b7f9172621ca
Sergey Teterlev <sergey.teterlev@gmail.com>
2025-09-21 09:28:21 +0300

    упростил код

--- go-tuts-purple-2/main.go
+++ go-tuts-purple-2/main.go
@@ -7,23 +7,20 @@ var transactions []float64
 func main() {
 	for {
 		transaction := fetchUserTransaction()
-		transactions = append(transactions, transaction)
 
-		fmt.Print("Добавить еще транзакцию? [y,n]: ")
-		var answer string = ""
-		fmt.Scan(&answer)
-
-		if answer != "y" {
+		if transaction == 0 {
 			break
 		}
+
+		transactions = append(transactions, transaction)
 	}
 
 	printResult()
 }
 
 func fetchUserTransaction() float64 {
-	fmt.Print("Введите число: ")
-	var value float64 = 0
+	fmt.Print("Введите число [0 для отмены]: ")
+	var value float64
 	fmt.Scan(&value)
 	return value
 }
